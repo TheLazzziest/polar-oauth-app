0ff605a6e383dd83521e72e644ca6ae49feffeca
TheLazzziest <mxyakovenko9@gmail.com>
2025-10-04 18:17:24 +0300

    Split the OAuth2 models for the service and the client

--- src/models.py
+++ src/models.py
@@ -1,12 +1,24 @@
+from abc import ABC
 from datetime import datetime
 
-from pydantic import BaseModel, PositiveInt
+from pydantic import BaseModel, Field, PositiveInt
 
 
-class TokenModel(BaseModel):
+class TemporalBaseModel(BaseModel, ABC):
+    created_at: datetime | None = Field(
+        None, description="Timestamp when the record was created."
+    )
+    updated_at: datetime | None = Field(
+        None, description="Timestamp when the record was last updated."
+    )
+
+
+class OAuth2TokenModel(BaseModel):
     access_token: str
     token_type: str
     expires_at: datetime
-    user_id: PositiveInt
-    updated_at: datetime
-    created_at: datetime
+    x_user_id: PositiveInt = Field(..., alias="user_id")
+
+
+class TokenModel(OAuth2TokenModel, TemporalBaseModel):
+    """Represents an OAuth2 token with user association and timestamps."""
