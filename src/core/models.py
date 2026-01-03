85719dd60257fc5cee80d3407710b0bc7af3634f
TheLazzziest <mxyakovenko9@gmail.com>
2026-01-03 16:16:10 +0300

    Bugfix, Add user models

--- src/core/models.py
+++ src/core/models.py
@@ -22,3 +22,38 @@ class OAuth2TokenModel(BaseModel):
 
 class TokenModel(OAuth2TokenModel, TemporalBaseModel):
     """Represents an OAuth2 token with user association and timestamps."""
+
+
+class UserExtraInfoModel(BaseModel):
+    """Represents extra information about a user."""
+
+    value: str | int | float | bool
+    index: int
+    name: str
+
+
+class UserModel(BaseModel):
+    polar_user_id: PositiveInt = Field(..., alias="polar-user-id")
+    member_id: PositiveInt = Field(..., alias="member-id")
+    registration_date: datetime = Field(
+        ...,
+        description="Timestamp when the user was registered.",
+        alias="registration-date",
+    )
+    first_name: str = Field(
+        ..., description="First name of the user.", alias="first-name"
+    )
+    last_name: str = Field(..., description="Last name of the user.", alias="last-name")
+    birth_date: datetime = Field(
+        ..., description="Timestamp when the user was born.", alias="birthdate"
+    )
+    gender: str = Field(..., description="Gender of the user.")
+    weight: float = Field(..., description="Weight of the user.")
+    height: float = Field(..., description="Height of the user.")
+    extra: list[UserExtraInfoModel] | None = Field(
+        None, description="Extra information about the user."
+    )
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
